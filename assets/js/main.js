32a0909fdfc03a1b1ab5ff163756e3504ef48de6
nachen98 <nachen98@gmail.com>
2022-12-10 10:32:50 -0800

    edits of title

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
 			$(
 				'<div id="titleBar">' +
 					// '<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + 'Welcome' + '</span>' +
+					'<span class="title">' + "Welcome" + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
